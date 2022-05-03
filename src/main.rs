9c7b52c8c0c371222654ae545476de612bbac360
Marcin Górny <marcin.gorny.94@protonmail.com>
2022-05-03 16:21:48 +0200

    Generate random vec and mult template

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use ark_ff::UniformRand;
 use ark_ff::{fields::Fp64, MontBackend, MontConfig, MontFp};
 use ark_ff::{BigInt, PrimeField};
+use ark_ff::{BigInteger64, UniformRand};
 use ark_std::rand::{
     distributions::{Distribution, Standard},
     Rng,
@@ -9,16 +9,18 @@ use ark_std::str::FromStr;
 use clap::Parser;
 use std::fs::File;
 use std::io::{BufRead, Error, ErrorKind};
-use std::ops::Deref;
 use std::ops::MulAssign;
+use std::ops::{Deref, Mul};
 use std::path::Path;
 use std::{fs, io};
 
 #[derive(MontConfig)]
-#[modulus = "17"]
+#[modulus = "9739"]
 #[generator = "3"]
 pub struct FqConfig;
 pub type Fq = Fp64<MontBackend<FqConfig, 1>>;
+
+type Vector = Vec<Fq>;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -31,19 +33,26 @@ struct Args {
 
     #[clap(short)]
     c_filename: String,
-
-    /// Prime field modulus
-    #[clap(short, long, default_value_t = 7)]
-    modulus: u64,
 }
 
-struct Matrix(Vec<Vec<Fq>>);
+struct Matrix(Vec<Vector>);
 /// impl Deref so we can access the anonymous
 /// field directly by calling an instance of the matrix
 impl Deref for Matrix {
-    type Target = Vec<Vec<Fq>>;
+    type Target = Vec<Vector>;
     fn deref(&self) -> &Self::Target {
         &self.0
+    }
+}
+
+impl Mul<&Vector> for &Matrix {
+    type Output = Vector;
+
+    fn mul(self, rhs: &Vector) -> Self::Output {
+        let mut result = Vector::new();
+        // self.into_iter()
+        //     .for_each(|x| x.into_iter().zip(rhs).map(|(a, b)| a * b));
+        result
     }
 }
 
@@ -55,18 +64,15 @@ fn main() {
         .map(|path| read_matrix(&path).unwrap())
         .collect();
 
-    let modulus = args.modulus;
-    // impl MontConfig<1>
-    // struct A();
-    let accept = verify(&matrices[0], &matrices[1], &matrices[2], modulus);
+    let accept = verify(&matrices[0], &matrices[1], &matrices[2]);
 }
 
 fn read_matrix(filename: &str) -> Result<Matrix, ()> {
     if let Ok(lines) = read_lines(filename) {
-        let mut rows: Vec<Vec<Fq>> = Vec::new();
+        let mut rows: Vec<Vector> = Vec::new();
         for line in lines {
             if let Ok(row) = line {
-                let row_string: Vec<Fq> = row
+                let row_string: Vector = row
                     .split(",")
                     .map(|elem: &str| Fq::from_str(elem).unwrap())
                     .collect();
@@ -89,26 +95,28 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn verify(a: &Matrix, b: &Matrix, c: &Matrix, p: u64) -> bool {
+fn verify(a: &Matrix, b: &Matrix, c: &Matrix) -> bool {
     // 1. pick a random r from the prime field
     let mut rng = ark_std::test_rng();
     let r: Fq = rng.gen();
 
     // 2. Compute the vector x = r, r^2, ...r^n
-    let mut x_vec: Vec<Fq> = Vec::with_capacity(a.len());
+    let mut x_vec: Vector = Vec::with_capacity(a.len());
     let mut temp: Fq = MontFp!(Fq, "1");
     for _ in 1..=a.len() {
         temp *= r;
         x_vec.push(temp);
     }
-    println!("Vec x is {:?}", x_vec);
+    // println!("Vec x is {:?}", x_vec);
 
     // 3. Multiply w=B.x
+    let w: Vector = b * &x_vec;
     // 4. Multiply A.w
+    let y: Vector = a * &w;
     // 5. Compute C.x
+    let z: Vector = c * &x_vec;
     // 6. Verify C.x == A.w
-
-    true
+    y == z
 }
 
 #[test]
